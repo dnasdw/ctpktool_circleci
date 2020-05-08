58e85b394119a1aa1d35510c72310e50e7e02666
Sun Daowen <sundaowen@gmail.com>
2020-05-08 15:12:09 +0800

    reusable

--- .cicommon/cmake_nmake_build.bat
+++ .cicommon/cmake_nmake_build.bat
@@ -1,7 +1,12 @@
-MD build
+MKDIR build
 PUSHD build
 SET CMAKE_BUILD_TYPE=%~1
 IF "%CMAKE_BUILD_TYPE%"=="" SET CMAKE_BUILD_TYPE=Release
-cmake -DCMAKE_BUILD_TYPE="%CMAKE_BUILD_TYPE%" -G "NMake Makefiles" ..
-cmake --build . --target install --config "%CMAKE_BUILD_TYPE%" --clean-first
+cmake -DCMAKE_BUILD_TYPE="%CMAKE_BUILD_TYPE%" -G "NMake Makefiles" .. || GOTO ERROR
+cmake --build . --target install --config "%CMAKE_BUILD_TYPE%" --clean-first || GOTO ERROR
 POPD
+GOTO :EOF
+
+:ERROR
+EXIT /B %ERRORLEVEL%
+GOTO :EOF
--- .cicommon/vs2019_command_prompt.bat
+++ .cicommon/vs2019_command_prompt.bat
@@ -1,7 +1,7 @@
 PUSHD "%~dp0"
 FOR /F %%I IN ('REG QUERY HKLM\SOFTWARE\WOW6432Node\Microsoft ^| FINDSTR "VisualStudio_"') DO CALL :FINDVS "%%~I" 2019
+IF NOT DEFINED VCVARSALL GOTO ERROR
 CALL %VCVARSALL% %1
-REM @ECHO ON
 POPD
 GOTO :EOF
 
@@ -9,4 +9,8 @@ GOTO :EOF
 SET vsid=%~1
 SET vsid=%vsid:~63%
 REG QUERY %~1\Capabilities | FINDSTR /R "ApplicationName.*REG_SZ.*Microsoft.Visual.Studio.%~2" && FOR /F "tokens=1,2,*" %%I IN ('REG QUERY HKLM\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\%vsid% ^| FINDSTR "InstallLocation"') DO SET VCVARSALL="%%~K\VC\Auxiliary\Build\vcvarsall.bat"
+GOTO :EOF
+
+:ERROR
+EXIT /B %ERRORLEVEL%
 GOTO :EOF
