58f0dd82950a49322a2bb6780b6a263a56890324
Sun Daowen <sundaowen@gmail.com>
2020-05-08 14:32:40 +0800

    reusable

--- .cicommon/cmake_nmake_build.bat
+++ .cicommon/cmake_nmake_build.bat
@@ -3,5 +3,5 @@ PUSHD build
 SET CMAKE_BUILD_TYPE=%~1
 IF "%CMAKE_BUILD_TYPE%"=="" SET CMAKE_BUILD_TYPE=Release
 cmake -DCMAKE_BUILD_TYPE="%CMAKE_BUILD_TYPE%" -G "NMake Makefiles" ..
-cmake --build . --target install --config Release --clean-first
+cmake --build . --target install --config "%CMAKE_BUILD_TYPE%" --clean-first
 POPD
